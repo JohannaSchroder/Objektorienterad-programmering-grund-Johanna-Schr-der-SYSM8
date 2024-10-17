ee65c200a3eece21f28cfda16f52901f9946a22e
JohannaSchroder <johannahell@hotmail.com>
2024-10-17 10:35:43 +0200

    fixat med klasserna Person, User och Admin. Saknas kod i metoderna i User och Admin.

--- FIT-TRACK/AdminUser.cs
+++ FIT-TRACK/AdminUser.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace FIT_TRACK
 {
-    class AdminUser
+    class AdminUser : User //Ärver från User
     {
+        //Egenskap
+        
+
+        //Konstruktor
+        public AdminUser(string UserName, string Password, string Country, string SecurityQuestion, string SecurityAnswer) 
+            : base(UserName, Password, Country, SecurityQuestion, SecurityAnswer) 
+        { 
+        }
+
+        //Metod
+        public override void SignIn()//HÄR BEHÖVER DU FYLLA I KOD!!
+        {
+            
+        }
+
+        public void MenageAllWorkouts()//HÄR BEHÖVER DU FYLLA I KOD!!
+        { 
+        
+        }
     }
 }
--- FIT-TRACK/User.cs
+++ FIT-TRACK/User.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace FIT_TRACK
 {
-    class User
+    class User : Person//Ärver från Person
     {
+        //egenskaper
+        public string Country { get; set; }
+        public string SecurityQuestion { get; set; }
+        public string SecurityAnswer { get; set; }
+
+        //konstruktor
+        public User(string UserName, string Password, string Country, string SecurityQuestion, string SecurityAnswer) : base(UserName, Password)
+        { 
+            this.Country = Country;
+            this.SecurityQuestion = SecurityQuestion;
+            this.SecurityAnswer = SecurityAnswer;
+        }
+
+        //metoder
+        public override void SignIn()//HÄR BEHÖVER DU LÄGGA IN KOD!!
+        {
+            
+        }
+
+        public void ResetPassword(string securityAnswer)
+        { 
+            
+        }
     }
 }
